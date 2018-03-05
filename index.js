67e9ea5b0ecc9cb3acee13e8223afc9b7a95506a
elshadaghayev <elshad@cryptomessage.io>
2018-03-05 10:21:25 +0400

    commit

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("port", (process.env.PORT || 5000));
-app.use(express.static(__dirname + "/app/public"));
+//app.use(express.static(__dirname + "/app/public"));
 
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./app/routing/apiRouting")(app);
 require("./app/routing/htmlRouting")(app);
+require("./app/routing/apiRouting")(app);
 
 app.listen(app.get("port"), function() {
   console.log("Listening on PORT " + app.get("port"));
